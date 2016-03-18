2b5010d8759c9f7a2ccf6f6c85df5a82de6b5b7c
Anamarija Radja <arada@neogov.net>
2016-03-18 10:18:48 -0700

    Adding Processor class which is meant to calculate mathematic operations (binary at this point)

---
+++ CalcIt/Processor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalcIt
+{
+    public class Processor
+    {
+        public double BinaryOperations(string operation,double operator1,double operator2)
+        {
+            switch (operation)
+            {
+                case "+":
+                    return operator1 + operator2;
+                case "-":
+                    return operator1 - operator2;
+                case "*":
+                    return operator1 * operator2;
+                case "/":
+                    return operator1 / operator2;
+                default:
+
+                   throw new Exception();
+            }
+        }
+    }
+}
