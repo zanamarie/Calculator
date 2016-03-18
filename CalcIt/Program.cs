5a1a09fe39fcdf6aba13aa021085ba28bcd4b9d5
Anamarija Radja <arada@neogov.net>
2016-03-18 10:17:14 -0700

    Adding methods for processing users input values. Method "AskAndProcessUsersAnswer" asks user to input value and exits if keyword exit is enetered. Method "AskUserForNumericValue" checks if user entered numberic value.

--- CalcIt/Program.cs
+++ CalcIt/Program.cs
@@ -8,8 +8,51 @@ namespace CalcIt
 {
     class Program
     {
+        public static string AskAndProcessUsersAnswer(string requirementStatement)
+        {
+            Console.WriteLine(requirementStatement);
+
+            var inputValue = Console.ReadLine();
+
+            if (inputValue.Contains("exit"))
+                Environment.Exit(0);
+
+            return inputValue;
+        }
+
+        public static double AskUserForNumericValue(string requirementStatement)
+        {
+            double parsedInputNumericValue;
+            do
+            {
+                var inputNumericValue = AskAndProcessUsersAnswer(requirementStatement);
+                if (!Double.TryParse(inputNumericValue, out parsedInputNumericValue))
+                {
+                    Console.WriteLine("String that you entered contains nonnumeric characters!");
+                }
+                else break;
+            }
+            while (true);
+
+            return parsedInputNumericValue;
+        }
+
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to CalcIt app. To exit CalcIt just type exit ");
+
+            while (true)
+            {
+                Processor calculation = new Processor();
+
+                var parsedFirstOperator = AskUserForNumericValue("Enter first number");
+                var parsedSecondOperator = AskUserForNumericValue("Enter second number");
+                var operation = AskAndProcessUsersAnswer("Enter operation between previously entered numbers");
+                var result = calculation.BinaryOperations(operation, parsedFirstOperator, parsedSecondOperator);
+
+                Console.WriteLine(parsedFirstOperator + operation + parsedSecondOperator + "=" + result);
+                Console.ReadKey();
+            }
         }
     }
 }
